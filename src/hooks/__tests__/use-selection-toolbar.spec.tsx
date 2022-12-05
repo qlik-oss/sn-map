3704027dbdfaa9d124fc93933e4e86369ba5b459
VasilisKalaitzis <cs101092@gmail.com>
2022-12-05 10:24:39 +0100

    feat(selections): add working lasso and circle selections

--- src/hooks/__tests__/use-selection-toolbar.spec.tsx
+++ src/hooks/__tests__/use-selection-toolbar.spec.tsx
@@ -11,7 +11,7 @@ describe('use-selection-toolbar', () => {
   let setEnabled: any;
   let active: boolean;
   let setActive: any;
-  let layout: LayerLayout;
+  let layout: MapLayout;
   let fn;
   let conditionArray;
   const Component = () => {
