3ffd478932f825de8aa05f2ff9fd247ccb1a2da0
Jens Jakobsson <bej@jenss-mbp.lan>
2023-01-31 16:00:44 +0100

    fix: tooltip visible

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Box, styled } from '@material-ui/core';
 import MeasureRow from './measure-row';
 
-const TooltipContent = styled(Box)(() => ({
+const TooltipContent = styled('div')((props) => ({
+  position: 'absolute',
   maxWidth: 368,
   padding: '8px 8px',
+  top: props?.position[0],
+  right: props?.position[1],
+  zIndex: 1000,
 }));
 
 export default function Tooltip({ model }) {
-  return <>{!model?.openTooltip && <div>{model?.content}</div>}</>;
+  return <>{model?.openTooltip && <TooltipContent position={model?.position}>{model?.content}</TooltipContent>}</>;
 }
--- src/hooks/use-tooltip.ts
+++ src/hooks/use-tooltip.ts
@@ -1,14 +1,36 @@
-import { useEffect, useState, useConstraints } from '@nebula.js/stardust';
+import { useEffect, useState, useConstraints, useRect } from '@nebula.js/stardust';
 
 const useTooltip = (mapModel: MapModelInterFace) => {
   const constraints = useConstraints();
+  const rect = useRect();
   const [openTooltip, setOpenTooltip] = useState(false);
   const [content, setContent] = useState('');
+  const [position, setPosition] = useState([]);
 
   useEffect(() => {
     if (!mapModel) {
       return;
     }
+
+    const calculatePosition = (features: idevio.map.Feature[], event: any) => {
+      let position = mapModel.map.geoToDisplay(features[0].getCenter());
+      const MULTI_CENTER_TOLERANCE_PX = 3;
+
+      for (let i = 1; i < features.length; i++) {
+        const center = mapModel.map.geoToDisplay(features[i].getCenter());
+        const distanceToFirstCenter = Math.sqrt((position[0] - center[0]) ** 2 + (position[1] - center[1]) ** 2);
+        if (distanceToFirstCenter > MULTI_CENTER_TOLERANCE_PX) {
+          // if the center points of all features are too far apart, just use pointer position
+          position = [event.clientX, event.clientY];
+          break;
+        }
+      }
+
+      position[0] = Math.max(0, Math.min(rect.width || 0, position[0]));
+      position[1] = Math.max(1, Math.min(rect.height || 0, position[1]));
+
+      return [position[0], position[1]];
+    };
 
     const onHover = (event: idevio.map.PositionEvent) => {
       if (event.target) {
@@ -19,6 +41,8 @@ const useTooltip = (mapModel: MapModelInterFace) => {
         setOpenTooltip(true);
         console.log('open', features);
         setContent(features[0].getAttribute('geoname'));
+        const pos = calculatePosition(features, event);
+        setPosition(pos);
       }
     };
 
@@ -33,13 +57,14 @@ const useTooltip = (mapModel: MapModelInterFace) => {
       mapModel.map.removeListener('move');
       mapModel.map.removeListener('out');
     };
-  }, [mapModel]);
+  }, [mapModel, rect]);
 
   useEffect(() => {}, [openTooltip]);
 
   return {
     openTooltip,
     content,
+    position,
   };
 };
 
